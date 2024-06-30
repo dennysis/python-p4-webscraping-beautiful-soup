85ec0ede0afd8c5b83b999b6f59fe0d035d04d35
partycrasher546 <muthonidennis546@gmail.com>
2024-06-30 17:56:37 +0300

    completed

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,13 @@
+
 from turtle import ht
 from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+print(doc.select('.heading-primary'))
+name = doc.select('.heading-primary')[0].name
+print(name)
+attr = doc.select('.heading-primary')[0].attrs
+print(attr)
